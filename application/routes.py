32bca8b8a7edb2fedf8266eb53e42ddb7a80c706
Radia Gulzan <radiagulzan@Radias-MacBook-Pro.local>
2022-07-13 12:05:00 +0100

    templates for the UI chosen and set up for further modification later

--- application/routes.py
+++ application/routes.py
@@ -2,6 +2,36 @@ from flask import Flask, url_for, render_template
 from application import app, db
 from application.models import *
 
+#templates provided by : copyright © Design template adopted from Gurupreeth Singh
+#to also mention in the ackowledgements part of README.md
 @app.route("/")
 def index():
-	return render_template('layout.html')
+	return render_template('home.html')
+
+@app.route("/login")
+def login():
+	return render_template('login.html')
+
+@app.route("/register")
+def register():
+	return render_template('register.html')
+
+@app.route("/dashboard")
+def dashboard():
+	return render_template('dashboard.html')
+
+@app.route("/addnewuser")
+def addnewuser():
+	return render_template('addnewuser.html')
+
+@app.route("/singleuser")
+def singleuserprofile():
+	return render_template('singleuser.html')
+
+@app.route("/update")
+def update():
+	return render_template('updateuser.html')
+
+@app.route("/logout")
+def logout():
+	return render_template('home.html')
