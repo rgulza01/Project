694faeb4b51ae998f1bc9cf280c113cea33bf29e
Radia Gulzan <radiagulzan@Radias-MacBook-Pro.local>
2022-07-14 13:11:28 +0100

    f string for DATABASE_URI in init.py

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY") 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = f"{os.getenv('DATABASE_URI')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
